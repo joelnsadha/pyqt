363643c9c67dd3d9ee6d77d1931e8d5b0083520e
joelnsadha <stonedev704@gmail.com>
2024-05-17 20:48:54 -0400

    Added widgets

--- main.py
+++ main.py
@@ -26,13 +26,44 @@ button1 = QPushButton("Choose Word")
 button2 = QPushButton("Choose Word")
 button3 = QPushButton("Choose Word")
 
+my_words = [
+    "hello",
+    "goodbye",
+    "test",
+    "app",
+    "python",
+    "pyqt",
+    "tall",
+    "here",
+]
+
 # -------------All designs-------------#
 # Create Layouts
 master_layout = QVBoxLayout()
+
 row1 = QHBoxLayout()
 row2 = QHBoxLayout()
 row3 = QHBoxLayout()
 
+# add title text to row1
+row1.addWidget(title, alignment=Qt.AlignCenter)
+
+# Add labels tto row2
+row2.addWidget(text1, alignment=Qt.AlignCenter)
+row2.addWidget(text2, alignment=Qt.AlignCenter)
+row2.addWidget(text3, alignment=Qt.AlignCenter)
+
+# add buttons to row3
+row3.addWidget(button1)
+row3.addWidget(button2)
+row3.addWidget(button3)
+
+# Add rows to master layout
+master_layout.addLayout(row1)
+master_layout.addLayout(row2)
+master_layout.addLayout(row3)
+
+main_window.setLayout(master_layout)
 # Events
 
 
